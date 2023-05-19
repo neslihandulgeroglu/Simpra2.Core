572654d0672d3422b3bae25b649118087f5b75ea
Neslihan <Neslihan@NESLI>
2023-05-19 22:40:35 +0300

    Add project files.

---
+++ Dto/StaffDto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APIProject.Core.Dto
+{
+    public class StaffDto
+
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public string Province { get; set; }
+
+    }
+}
---
+++ UnitOfWork/IunitOfWork.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APIProject.Core.UnitOfWork
+{
+    public  interface IunitOfWork
+    {
+        Task CommitAsync();
+        void Commit();
+    }
+}
